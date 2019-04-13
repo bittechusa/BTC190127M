15d71a4f3a24c44a4e4233ab556f996a0e4f68f8
my name <you@examplejtge.com>
2019-04-13 14:16:50 -0400

    wjhgdch

---
+++ BTC190127M_MavenProject/src/test/java/com/test/SmokeTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.junit.Test;
+
+public class SmokeTest {
+	
+	@Test
+	public void sampleTestMethod() {
+		System.out.println("___________________");
+		System.out.println("This is maven Test");
+		System.out.println("___________________");
+	}
+	
+	@Test
+	public void sampleTestMethod1() {
+		System.out.println("___________________");
+		System.out.println("ano ther test");
+		System.out.println("___________________");
+	}
+
+}
