37b31a63b3c54fac93c1b22a4518186cca61c432
my name <you@examplejtge.com>
2019-05-26 14:20:11 -0400

    TXT File Read

--- TestNGProject_BTC190127M/src/main/java/com/bit/dr/ReadData.java
+++ TestNGProject_BTC190127M/src/main/java/com/bit/dr/ReadData.java
@@ -1,20 +1,54 @@
 package com.bit.dr;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ReadData 
 {
+	
+	
+	public StringBuffer readTXT(String path)
+	{
+		StringBuffer sb=new StringBuffer();
+		try {
+			FileReader f=new FileReader(new File(path));
+			BufferedReader bf=new BufferedReader(f);
+			String line=bf.readLine();
+			while(line!=null)
+			{
+				System.out.println(line);
+				sb.append(line).append(System.lineSeparator());
+				line=bf.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sb;
+	}
+	
+	
+	
+	
+	
+	
 	@DataProvider(name="login")
 	public Object[][] getData1()
 	{
@@ -49,50 +83,94 @@ public class ReadData
 	@Test(dataProvider="value")
 	public void test3(Object u,Object p)
 	{
+		readTXT("/Users/bittechconsulting/eclipse-workspace/new/TestNGProject_BTC190127M/file1.txt");
+		readTXT("\"/Users/bittechconsulting/eclipse-workspace/new/TestNGProject_BTC190127M/file1.txt\"")
 		System.out.println(u+"+"+p);
 		dr.findElement(By.xpath("")).sendKeys(u.toString());
 	}
 	
 	@DataProvider(name="value")
-	public Object[][] readxl() throws IOException
+	public Object[][] readxl() 
 	{
-		FileInputStream f=new FileInputStream(new File("/Users/bittechconsulting/eclipse-workspace/new/TestNGProject_BTC190127M/book.xlsx"));
-		XSSFWorkbook book=new XSSFWorkbook(f);
-		XSSFSheet sheet=book.getSheetAt(0);
-		sheet.getRow(1).getCell(2).getStringCellValue();//to get one cell vale
+		FileInputStream f = null;
+		XSSFWorkbook book = null;
+		XSSFSheet sheet = null;
+		Object [][]  datas = null;
+		try {
+			f = new FileInputStream(new File("/Users/bittechconsulting/eclipse-workspace/new/TestNGProject_BTC190127M/book.xlsx"));
+		   
+			book=new XSSFWorkbook(f);
+			sheet=book.getSheetAt(0);
+			sheet.getRow(1).getCell(2).getStringCellValue();//to get one cell vale
 		
 		
 		 
-		int rowSize=sheet.getLastRowNum();
-		int celSize=sheet.getRow(0).getLastCellNum();
-		Object [][] datas=new Object[rowSize][celSize];
+			int rowSize=sheet.getLastRowNum();
+			int celSize=sheet.getRow(0).getLastCellNum();
+			datas=new Object[rowSize][celSize];
 		
-		for(int i=1;i<rowSize;i++)
-		{
-			for(int j=0;j<celSize;j++)
+			for(int i=1;i<rowSize;i++)
 			{
-				XSSFCell c=sheet.getRow(i).getCell(j);
-				switch(c.getCellType())
+				for(int j=0;j<celSize;j++)
 				{
-				case XSSFCell.CELL_TYPE_NUMERIC:
-				{
+					XSSFCell c=sheet.getRow(i).getCell(j);
+					switch(c.getCellType())
+					{
+					case XSSFCell.CELL_TYPE_NUMERIC:
+					{
 					datas[i-1][j]=c.getNumericCellValue();
 					break;
-				}
-				case XSSFCell.CELL_TYPE_STRING:
-				{
+					}
+					case XSSFCell.CELL_TYPE_STRING:
+					{
 					datas[i-1][j]=c.getStringCellValue();
 					break;
-				}
+					}
 					
+					}
+				
+				
+				
 				}
-				
-				
-				
+			}
+		
+			} catch (FileNotFoundException e) {
+			e.getMessage();
+			System.out.println("file not found");
+			}
+			catch (IOException e) {
+			e.getMessage();
+			System.out.println("IO exception");
+			}
+			catch (NoSuchElementException e) {
+			e.getMessage();
+			System.out.println("element not found exception");
+			}
+			catch (Exception e) {
+			e.getMessage();
+			System.out.println("not matched");
+			}
+		finally {
+			if(book!=null)
+			{
+				try {
+					book.close();
+				} catch (IOException e) {
+					System.out.println("book is not initialized");
+				}
+			}
+			if(f!=null)
+			{
+				try {
+					f.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 		
-		
+		System.out.println("hi");
 		
 		return datas;
 	}
